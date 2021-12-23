f2d371225a669f45699acbe7c7a8884b2d131763
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2021-12-23 18:41:36 +0200

    Add first version of the app logic
    - Imlement MVC design pattern

---
+++ QuizyApp/Controller/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  QuizyApp
+//
+//  Created by Kostadin Samardzhiev on 22.12.21.
+//
+
+import UIKit
+
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+
+        return nil
+    }
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = (sender.titleLabel?.text!)!
+        
+        if quizBrain.checkAnswer(userAnswer) {
+            print("Right!")
+            sender.backgroundColor = UIColor.init(hex: "#94D0CCFF")
+        } else {
+            print("Wrong!")
+            sender.backgroundColor = UIColor.init(hex: "#F29191FF")
+        }
+        
+        quizBrain.nextQuestion()
+
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        
+    }
+    
+}
+
---
+++ QuizyApp/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  QuizyApp
+//
+//  Created by Kostadin Samardzhiev on 23.12.21.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
